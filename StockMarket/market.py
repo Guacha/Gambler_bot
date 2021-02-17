e917c1ba8bbef16f42032407cb206bf32bf23e60
Guacha <guachamalinga@gmail.com>
2021-02-17 14:11:11 -0500

    Stock initialization completed

--- StockMarket/market.py
+++ StockMarket/market.py
@@ -1,6 +1,14 @@
+from google.cloud.firestore_v1.document import DocumentReference
+
 class StockMarket:
 
     def __init__(self, db):
-        print(f"Inicializando submódulo {__name__}")
+        print(f"\tInicializando submódulo {__name__}")
         self.__db = db
-        self.stock_refs: list = db.collection("config").document("bot_config").collection("stonks").list_documents()
+        self.stock_refs: "dict[str:DocumentReference]" = {doc.id: doc for doc in db.collection("config").document("bot_config").collection("stonks").list_documents()}
+        print("\tMódulo cargado con éxito")
+        
+    def setup_stock(self, symbol: str, data: dict):
+    
+        symbol_doc = self.stock_refs[symbol]
+        symbol_doc.update(data)
--- bot.py
+++ bot.py
@@ -1,4 +1,21 @@
 from DB_Control import db
 from StockMarket.market import StockMarket
+import json
 
 stonks = StockMarket(db)
+
+with open("dump.json") as file:
+    dump = json.load(file)
+    for symbol, info in dump.items():
+        payload = {
+            "name": info['name'],
+            "market": info['market'],
+            "website": info['website'],
+            "total_stock_amount": info["vol"],
+            "per_stock_price": info["per_stock_price"]
+        }
+        stonks.setup_stock(symbol, payload)
+
+
+    
+    
