930b7f8f4befd57cc1eaa9a980ff16cce843b9c4
Guacha <guachamalinga@gmail.com>
2021-02-20 10:04:19 -0500

    Stocks agregadas a DB TODO: agregar funciones de manejo de precios

--- StockMarket/market.py
+++ StockMarket/market.py
@@ -5,10 +5,25 @@ class StockMarket:
     def __init__(self, db):
         print(f"\tInicializando submódulo {__name__}")
         self.__db = db
-        self.stock_refs: "dict[str:DocumentReference]" = {doc.id: doc for doc in db.collection("config").document("bot_config").collection("stonks").list_documents()}
+        self.load_stock_refs()
         print("\tMódulo cargado con éxito")
+    
         
     def setup_stock(self, symbol: str, data: dict):
+        try:
+            symbol_doc = self.stock_refs[symbol]
+            symbol_doc.update(data)
+        except KeyError:
+            symbol_doc = self.__db.collection("config").document("bot_config").collection("stonks").document(symbol).set(data)
+        
+        self.load_stock_refs()
     
-        symbol_doc = self.stock_refs[symbol]
-        symbol_doc.update(data)
+    
+    def load_stock_refs(self) -> "dict[str:DocumentReference]":
+        self.stock_refs = {doc.id: doc for doc in self.__db.collection("config").document("bot_config").collection("stonks").list_documents()}
+    
+    
+    def get_stock_price(self, symbol: str) -> float:
+        stock = self.stock_refs[symbol].get()
+        return stock.to_dict()['per_stock_price']
+        
--- bot.py
+++ bot.py
@@ -1,30 +1,5 @@
 from DB_Control import db
 from StockMarket.market import StockMarket
-import json
 
-stonks = StockMarket(db)
-
-# TODO: Agregar las categorías a Firebase
-# NOTE: Esta funcion es temporal, y solo cumple el propósito del setup inicial
-
-categories = (
-    ""
-)
-with open("dump.json") as file:
-    dump = json.load(file)
-    for symbol, info in dump.items():
-        cats = input(f"Categorías de {info['name']}: ")
-        list_cats = [categories[int(i)] for i in cats.split(",")]
-        payload = {
-            "name": info['name'],
-            "market": info['market'],
-            "website": info['website'],
-            "total_stock_amount": info["vol"],
-            "per_stock_price": info["per_stock_price"],
-            "categories": list_cats,
-        }
-        stonks.setup_stock(symbol, payload)
-
-
-    
+stonks = StockMarket(db) 
     
